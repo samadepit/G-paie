241abbaf36201447c5f42250d69a5bfe198891ad
nunuel001 <emmanuel.oulaiepitech.eu>
2024-09-19 15:54:11 +0000

    bon5 retour

--- Prozect/MachineLearning/Project/APIProjectFolder/sender.py
+++ Prozect/MachineLearning/Project/APIProjectFolder/sender.py
@@ -1,7 +1,7 @@
 import sqlite3
 import pandas as pd
 
-df = pd.read_csv('APIProjectFolder/produits_fcfa_expiration(2).csv')
+df = pd.read_csv('Prozect/MachineLearning/Project/APIProjectFolder/produits_fcfa_expiration(2).csv')
 
 Connection = sqlite3.connect('db.sqlite3')
 
