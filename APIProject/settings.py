a122da9ab5c8859677b85f7c6619a10fee883647
samadepit <salif.nombre@epitech.eu>
2024-09-23 19:56:42 +0000

    render_netyoiye_push

--- APIProject/settings.py
+++ APIProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-p%gd3z^yh)b7e5mv#a6gt_+#(_vc(6i2jz^fl!=nu^f^ad0bk^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['rest-auth.onrender.com']
 
 
 # Application definition
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.whiteNoiseMiddleware"
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -116,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+if not DEBUG :
+    STATIC_ROOT = os.path.join(BASE_BIR,'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompresseManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
