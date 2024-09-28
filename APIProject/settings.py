45f19b62e79a0e44e95a3611597a4e95a1eea806
samadepit <salif.nombre@epitech.eu>
2024-09-28 16:21:44 +0000

    nettoyage_aborescence

--- APIProject/settings.py
+++ APIProject/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import MySQLdb as Database
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
